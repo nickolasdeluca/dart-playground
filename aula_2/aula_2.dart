5d5424b7d5bf906d6c4b002e8abedb660626d0a8
Nickolas de Luca Alberton <nickolasdeluca@live.com>
2022-05-13 21:10:33 -0300

    Exemplo com classes, interpolação, construtores

---
+++ aula_2/aula_2.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+class Pessoa {
+  String nome;
+  double altura;
+  double peso;
+  double? imc;
+
+  Pessoa(this.nome, this.altura, this.peso);
+
+  void calcularIMC() {
+    imc = (peso / pow(altura, 2));
+  }
+
+  toJson() {
+    return {
+      'Nome': this.nome,
+      'Altura': this.altura,
+      'Peso': this.peso,
+      'IMC': this.imc
+    };
+  }
+}
+
+void main(List<String> args) {
+  List<Pessoa> pessoas = [];
+  pessoas.add(Pessoa('Nickolas', 1.75, 94));
+  pessoas.add(Pessoa('Vanessa', 1.64, 50));
+
+  pessoas.forEach((element) {
+    element.calcularIMC();
+    print(element.toJson());
+  });
+}
